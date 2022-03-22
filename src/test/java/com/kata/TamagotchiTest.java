ffe6c79b1d87d78015827ad5bdf5e31b855fa5ce
Anaroco <roblescortes@msn.com>
2022-03-22 21:08:49 +0100

    test done

--- src/test/java/com/kata/TamagotchiTest.java
+++ src/test/java/com/kata/TamagotchiTest.java
@@ -12,4 +12,52 @@ public class TamagotchiTest {
         assertTrue( true );
     }
 
+    @Test
+    public void test_tamagotchi_mood()
+    {
+        // Given
+        Tamagotchi tamagotchi = new Tamagotchi();
+        int expected = tamagotchi.getMood();
+
+        // When
+        int actual = 4;
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    private void assertEquals(int expected, int actual) {
+    }
+
+    @Test
+    public void test_tamagotchi_hunger()
+    {
+        // Given
+        Tamagotchi tamagotchi = new Tamagotchi();
+        int expected = tamagotchi.getHunger();
+
+        // When
+        int actual = 4;
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_tamagotchi_energy()
+    {
+        // Given
+        Tamagotchi tamagotchi = new Tamagotchi();
+        int expected = tamagotchi.getEnergy();
+
+        // When
+        int actual = 4;
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
 }
+
+
+
